bea165cc2d55cceb0346d378b4a52ee05197b758
Chris Shields <christopher.shields143@gmail.com>
2021-02-23 19:18:11 -0800

    use gunicorn specific arguments? 'respond' with status code?

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
-def whatever(*args):
+def whatever(env, resp):
+    resp('200 OK', [('Content-Type', 'text/plain')])
     return [b'hello, world']
