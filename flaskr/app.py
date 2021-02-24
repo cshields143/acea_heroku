c1ad8a3dd1f7f0e11c794a0dfc026fd880ffea54
Chris Shields <christopher.shields143@gmail.com>
2021-02-24 13:50:18 -0800

    point app to new template-y home page

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
     
     @app.route('/')
     def root():
-        return 'hello, world'
+        return render_template('root.html')
     
     return app
