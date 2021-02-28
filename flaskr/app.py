187bd08781dfc88dea89ea536e846c11c1e1a92c
Chris Shields <christopher.shields143@gmail.com>
2021-02-27 20:41:12 -0800

    add route for imputation page

--- flaskr/app.py
+++ flaskr/app.py
@@ -11,4 +11,8 @@ def create_app():
     def about():
         return render_template('about.html', title='Project Overview - Blue Gold')
     
+    @app.route('/cleaning')
+    def imput():
+        return render_template('imput.html', title='Cleaning & Imputation - Blue Gold')
+    
     return app
